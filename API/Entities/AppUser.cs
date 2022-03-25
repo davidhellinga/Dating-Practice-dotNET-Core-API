cc75b7244d22caf4ee06b2d15816966672d10c8b
David Hellinga <d.hellinga@student.fontys.nl>
2022-03-25 10:02:56 +0100

    Added wwwroot to git so Heroku has it

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -8,9 +8,10 @@ public class AppUser : IdentityUser<int>
 
     public string KnownAs { get; set; }
 
-    public DateTime Created { get; set; } = DateTime.Now;
+    public DateTime Created { get; set; } = DateTime.UtcNow;
 
-    public DateTime LastActive { get; set; } = DateTime.Now;
+    
+    public DateTime LastActive { get; set; } = DateTime.UtcNow;
 
     public string Gender { get; set; }
 
